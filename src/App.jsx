2ca458c5184753efbbcddd2082916ea2f6b28709
Iryna Klyonova <iro4ka240404@gmail.com>
2025-03-11 01:41:07 +0100

    Refactor ShopCategories to receive data via props

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,28 @@ import NEWCollection from "./new-collection/NewCollection";
 import AboutUs from "./about-block/AboutUs";
 import FollowUs from "./submit-form/FollowUs";
 
+const shopCategoriesData = {
+  title: "SHOP BY CATEGORIES",
+  images: [
+    { src: "/assets/Categories1.svg", className: "image-1", alt: "Category 1" },
+    { src: "/assets/Categories2.svg", className: "image-2", alt: "Category 2" },
+    { src: "/assets/Categories3.svg", className: "image-3", alt: "Category 3" },
+    { src: "/assets/Categories4.svg", className: "image-4", alt: "Category 4" },
+  ],
+  longImage: { src: "/assets/Categories5.svg", className: "long-image", alt: "Long Category" }
+};
+
 function App() {
-  return <div className="App">
-    <Header />
-    <OrganicFashion />
-    <ShopCategories />
-    <NEWCollection />
-    <AboutUs />
-    <FollowUs />
-  </div>;
+  return (
+    <div className="App">
+      <Header />
+      <OrganicFashion />
+      <ShopCategories {...shopCategoriesData} />
+      <NEWCollection />
+      <AboutUs />
+      <FollowUs />
+    </div>
+  );
 }
 
 export default App;
--- src/categories-block/CategoriesBlock.jsx
+++ src/categories-block/CategoriesBlock.jsx
@@ -1,16 +1,15 @@
 import './CategoriesBlock.scss';
 
-const ShopCategories = () => {
+const ShopCategories = ({ title, images, longImage }) => {
   return (
     <div className="shop-categories">
-      <h2 className="shop-title">SHOP BY CATEGORIES</h2>
+      <h2 className="shop-title">{title}</h2>
       <div className="image-container">
-        <img src="/assets/Categories1.svg" className="image-1" alt="Category 1" />
-        <img src="/assets/Categories2.svg" className="image-2" alt="Category 2" />
-        <img src="/assets/Categories3.svg" className="image-3" alt="Category 3" />
-        <img src="/assets/Categories4.svg" className="image-4" alt="Category 4" />
+        {images.map((image, index) => (
+          <img key={index} src={image.src} className={image.className} alt={image.alt} />
+        ))}
       </div>
-      <img src="/assets/Categories5.svg" className="long-image" alt="Long Category" />
+      <img src={longImage.src} className={longImage.className} alt={longImage.alt} />
     </div>
   );
 };
